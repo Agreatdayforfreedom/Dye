f49100eaec4b9582c7b8ec23f0e4fdaec2678f34
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-10-20 18:03:22 -0300

    saturation

--- app/_components/LerpColors.tsx
+++ app/_components/LerpColors.tsx
@@ -8,23 +8,7 @@ import { rgba2hsv } from "../_utils/rgba2hsv";
 import { hsv2rgba } from "../_utils/hsv2rgba";
 import { RgbaColor, RgbaColorPicker } from "react-colorful";
 import { ColorPicker } from "./ColorPicker";
-
-// function hsv_to_hsl(h: number, s: number, v: number) {
-//   // both hsv and hsl values are in [0, 1]
-//   var l = ((2 - s) * v) / 2;
-
-//   if (l != 0) {
-//     if (l == 1) {
-//       s = 0;
-//     } else if (l < 0.5) {
-//       s = (s * v) / (l * 2);
-//     } else {
-//       s = (s * v) / (2 - l * 2);
-//     }
-//   }
-
-//   return { h, s, l };
-// }
+import chroma from "chroma-js";
 
 let initializeLeftColor = (): RgbaColor => {
   return {
@@ -45,7 +29,8 @@ let initializeRightColor = (): RgbaColor => {
 export const LerpColors = () => {
   const [leftColor, setLeftColor] = useState<RgbaColor>(initializeLeftColor);
   const [rightColor, setRightColor] = useState<RgbaColor>(initializeRightColor);
-
+  const [saturation, setSaturation] = useState<number>(0);
+  const [saturationFactor, setSaturationFactor] = useState(1);
   let a = new RGBA(
     leftColor.r,
     leftColor.g,
@@ -58,54 +43,41 @@ export const LerpColors = () => {
     rightColor.b,
     (rightColor.a || 1) * 255
   );
-  let values: RGBA[] = [];
   let steps = 20;
-  for (let i = 0; i < steps - 1; i++) {
-    values.push(lerpRGBA(a, b, i / steps));
-  }
-  let values_hsv: HSV[] = [];
-  let ah = rgba2hsv(a);
-  let bh = rgba2hsv(b);
-  for (let i = 0; i < steps - 1; i++) {
-    values_hsv.push(lerpHSV(ah, bh, i / steps));
-  }
+
+  // todo: set pointer and fix the saturation
+  let last = chroma
+    .scale([chroma.rgb(a.r, a.g, a.b), chroma.rgb(b.r, b.g, b.b)])
+    .mode("rgb")
+    .colors(steps)
+    .map((x) => chroma.hex(x).saturate(saturation));
 
   return (
     <div className="size-full flex items-end justify-center">
       <ColorPicker color={leftColor} setColor={setLeftColor} />
       <div>
         <div className="flex">
-          {values.map((c) => {
-            return (
-              <div>
+          <div className="flex">
+            {last.map((c: any) => {
+              return (
                 <div
                   style={{
-                    background: formatRGBA(c),
+                    background: c,
                     width: `${500 / steps}px`,
                   }}
                   className="h-16"
                 />
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-        <div className="flex">
-          {values_hsv.map((c: any) => {
-            return (
-              <div
-                style={{
-                  background: formatRGBA(hsv2rgba(c)),
-                  width: `${500 / steps}px`,
-                }}
-                className="h-16"
-              />
-            );
-          })}
-        </div>
+        <ColorPicker color={rightColor} setColor={setRightColor} />
+        <input
+          type="number"
+          value={saturation}
+          onChange={(e) => setSaturation(parseInt(e.target.value))}
+        />
       </div>
-      <ColorPicker color={rightColor} setColor={setRightColor} />
     </div>
-
-    // </div>
   );
 };
