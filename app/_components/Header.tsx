6580f5371aaccd7d3820406667c591ab25c2993d
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-10-22 20:02:08 -0300

    header title and colors

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -3,11 +3,20 @@ import React from "react";
 import { useGlobalDyes } from "../store/global_dyes";
 
 export const Header = () => {
-  const dye = useGlobalDyes((state) => state.dye1);
+  const dye1 = useGlobalDyes((state) => state.dye1);
+  const dye5 = useGlobalDyes((state) => state.dye5);
   return (
-    <div
-      className="h-12 w-full border-b border-gray-700"
-      style={{ background: dye }}
-    ></div>
+    <header className="h-12 w-full border-b" style={{ borderColor: dye5 }}>
+      <div className="h-full w-11/12 flex items-center mx-auto">
+        <div className="flex space-x-1 items-end">
+          <h1 style={{ color: dye1 }} className="text-xl font-bold">
+            Dye
+          </h1>
+          <p className="text-sm font-semibold" style={{ color: dye1 }}>
+            Palette generator
+          </p>
+        </div>
+      </div>
+    </header>
   );
 };
