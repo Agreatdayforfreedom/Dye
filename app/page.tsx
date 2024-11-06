571b02fcebcc01f6508616c8a5572377370b3fd2
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-11-06 19:18:02 -0300

    useSearchParams suspende

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import { LerpColors } from "./_components/LerpColors";
 
 export default function Home() {
   return (
     <main className="h-full">
-      <LerpColors />
+      <Suspense fallback="loading...">
+        <LerpColors />
+      </Suspense>
     </main>
   );
 }
