b3c8f1db87327b21c3f987dd06ef7c10e222c4fc
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-11-08 18:28:23 -0300

    chart preview component

--- app/_components/preview_components/PreviewSection.tsx
+++ app/_components/preview_components/PreviewSection.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Search } from "./Search";
+import { Chart } from "./Chart";
 
 const PreviewSection = () => {
   return (
     <div className="flex sm:max-w-[90%] mx-auto mt-5">
       <Search />
+      <Chart />
     </div>
   );
 };
