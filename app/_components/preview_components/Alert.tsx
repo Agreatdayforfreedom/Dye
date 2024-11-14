9812a8e54986419984ea9289ccdf06ba63df91dc
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-11-14 16:06:26 -0300

    add alert preview component

---
+++ app/_components/preview_components/Alert.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useGlobalDyes } from "@/app/store/global_dyes";
+import { AlertCircle, AlertOctagonIcon } from "lucide-react";
+import { getContrastYIQ } from "../../_utils/yiq";
+
+export const Alert = () => {
+  const c1 = useGlobalDyes((state) => state.title_dye);
+
+  return (
+    <div
+      style={{ background: c1 }}
+      className="h-12 rounded  flex items-center space-x-2 px-2"
+    >
+      <AlertOctagonIcon style={{ stroke: getContrastYIQ(c1) }} />
+      <span className="font-bold" style={{ color: getContrastYIQ(c1) }}>
+        Alert
+      </span>
+    </div>
+  );
+};
--- app/_components/preview_components/PreviewSection.tsx
+++ app/_components/preview_components/PreviewSection.tsx
@@ -3,18 +3,20 @@ import { Search } from "./Search";
 import { Chart } from "./Chart";
 import { CheckList } from "./CheckList";
 import { TextCard } from "./TextCard";
+import { Alert } from "./Alert";
 
 const PreviewSection = () => {
   return (
-    <div>
+    <div className="max-w-[95%] sm:max-w-[90%] mx-auto">
       <Search />
-      <div className="flex mt-5 space-x-4">
-        <div className="flex flex-col  sm:max-w-[90%]">
+      <div className="text-center space-y-4 sm:flex mt-5 space-x-4 justify-between">
+        <div className="sm:flex flex-col ">
           <Chart />
         </div>
         <div className="flex flex-col space-y-2">
-          <CheckList />
+          <Alert />
           <TextCard />
+          <CheckList />
         </div>
       </div>
     </div>
