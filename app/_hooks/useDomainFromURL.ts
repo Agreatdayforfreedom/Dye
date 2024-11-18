8e96989ad279424b5746976242c974cab3188535
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-11-18 18:29:21 -0300

    more url validations

--- app/_hooks/useDomainFromURL.ts
+++ app/_hooks/useDomainFromURL.ts
@@ -1,8 +1,14 @@
 import { useSearchParams } from "next/navigation";
 import { DomainLayout } from "../types";
-import { default_tw_color_domains } from "../constants";
+import { default_tw_color_domains } from "@/app/constants";
 import chroma from "chroma-js";
-import { validate_hex, validate_indices } from "../_utils/validators";
+import { validate_hex, validate_indices } from "@/app/_utils/validators";
+
+// this maybe can be randomly generated, or selected from tw_color_domains, idk.
+const default_domain: DomainLayout = {
+  hex: ["#cccccc", "#23901e"],
+  indices: [0, 10],
+};
 
 export const useDomainFromURL = (): DomainLayout => {
   const searchParams = useSearchParams();
@@ -10,9 +16,15 @@ export const useDomainFromURL = (): DomainLayout => {
   const name = searchParams.get("name") || "mellow berry";
   const as_palette = default_tw_color_domains[name.split(" ").join("_")];
 
-  //todo fix parsing JSON errors
-  let hex: string[] = JSON.parse(searchParams.get("p") || "[]");
-  let indices: number[] = JSON.parse(searchParams.get("i") || "[]");
+  let hex: string[] = [];
+  let indices: number[] = [];
+  try {
+    hex = JSON.parse(searchParams.get("p") || "[]");
+    indices = JSON.parse(searchParams.get("i") || "[]");
+  } catch {
+    hex = default_domain.hex;
+    indices = default_domain.indices;
+  }
 
   const valid_hex = validate_hex(hex);
   const valid_indices = validate_indices(indices);
@@ -22,7 +34,7 @@ export const useDomainFromURL = (): DomainLayout => {
     indices: [],
   };
 
-  if (valid_hex && valid_indices) {
+  if (valid_hex && valid_indices && hex.length === indices.length) {
     domain = {
       hex,
       indices,
@@ -31,8 +43,7 @@ export const useDomainFromURL = (): DomainLayout => {
     domain.hex = as_palette.hex;
     domain.indices = as_palette.indices;
   } else {
-    domain.hex = ["#cccccc", "#23901e"];
-    domain.indices = [0, 10];
+    domain = default_domain;
   }
 
   return domain;
