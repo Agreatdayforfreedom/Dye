c6228e8c8d3cfef1630fa95109b97ac7d245a9ec
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-11-19 18:21:37 -0300

    better test cases for useDomainFromURL hook

--- __test__/useDomainFromURL.spec.ts
+++ __test__/useDomainFromURL.spec.ts
@@ -12,41 +12,33 @@ import { default_tw_color_domains } from "../app/constants";
 import { DomainLayout } from "../app/types";
 import { match } from "assert";
 
-vi.mock("../app/_utils/validators", () => ({
-  validate_hex: vi.fn(),
-  validate_indices: vi.fn(),
-}));
-
 vi.mock("next/navigation", () => ({
   useSearchParams: vi.fn(),
 }));
 
-const DEFAULT_PALETTE_DOMAIN = default_domain;
 const FROM_TW_DEFAULT_PALETTE_DOMAIN_NAME = "mellow_berry";
 
-const setup = (valid_hex: boolean, valid_indices: boolean, params: string) => {
-  (validate_hex as Mock).mockReturnValue(valid_hex);
-  (validate_indices as Mock).mockReturnValue(valid_indices);
+const setup = (params: string) => {
   (useSearchParams as Mock).mockReturnValue(new URLSearchParams(params));
 };
 
-afterEach(() => {
-  vi.restoreAllMocks();
+test("should return default (mellow berry) color palette domain from default_tw_color_domains if the query params are invalid", () => {
+  let cases = ["?p=[]&i=[]", "?p=[]", "?p", "?i=[]", "kkkkkkkkkkkkkkk"];
+
+  for (const test of cases) {
+    setup(test);
+
+    const { result } = renderHook(() => useDomainFromURL());
+
+    expect(result.current).toEqual({
+      hex: default_tw_color_domains[FROM_TW_DEFAULT_PALETTE_DOMAIN_NAME].hex,
+      indices:
+        default_tw_color_domains[FROM_TW_DEFAULT_PALETTE_DOMAIN_NAME].indices,
+    });
+  }
 });
 
-test("should return default (mellow berry) color palette domain from default_tw_color_domains if the query params are empty", () => {
-  setup(false, false, "");
-
-  const { result } = renderHook(() => useDomainFromURL());
-
-  expect(result.current).toEqual({
-    hex: default_tw_color_domains[FROM_TW_DEFAULT_PALETTE_DOMAIN_NAME].hex,
-    indices:
-      default_tw_color_domains[FROM_TW_DEFAULT_PALETTE_DOMAIN_NAME].indices,
-  });
-});
-
-test("should return default color palette domain if the query params are invalid", () => {
+test("should return default color palette domain if the query params are invalid and the name is present but does not match with existing domain in [default_tw_color_domains]", () => {
   let cases = [
     "?name=invalid",
     "?p=[]&name=invalid",
@@ -56,7 +48,7 @@ test("should return default color palette domain if the query params are invalid
   ];
 
   for (const c of cases) {
-    setup(false, false, c);
+    setup(c);
 
     const { result } = renderHook(() => useDomainFromURL());
 
@@ -69,6 +61,10 @@ test("should return default color palette domain if the query params are invalid
 
 test("should return the same params if the domain is valid", () => {
   let cases = [
+    {
+      params: '?p=["#000"]&i=[10]',
+      match: { hex: ["#000"], indices: [10] },
+    },
     {
       params: '?p=["#000", "#fff"]&i=[0, 10]',
       match: { hex: ["#000", "#fff"], indices: [0, 10] },
@@ -90,7 +86,7 @@ test("should return the same params if the domain is valid", () => {
   ];
 
   for (const obj of cases) {
-    setup(true, true, obj.params);
+    setup(obj.params);
     const { result } = renderHook(() => useDomainFromURL());
 
     expect(result.current).toEqual(obj.match);
