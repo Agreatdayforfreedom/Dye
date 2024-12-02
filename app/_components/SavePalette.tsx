6a7ce0938e82a8c1ab0b113433b7f00c4a0e9951
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-12-02 16:26:29 -0300

    Show palette changes when the save palette alert card is opened

--- app/_components/SavePalette.tsx
+++ app/_components/SavePalette.tsx
@@ -19,6 +19,14 @@ import { useGlobalDyes } from "@/app/store/global_dyes";
 import { useDarkMode } from "@/app/store/dark_mode";
 import { cn } from "@/lib/utils";
 import { useToast } from "@/hooks/use-toast";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../../components/ui/accordion";
+import { Attributes } from "../types";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 export const SavePalette = () => {
   const { setDomain, exists, is_equal } = usePersistentStore();
@@ -73,6 +81,7 @@ export const SavePalette = () => {
         Save
       </AlertDialogTrigger>
       {is_equal(name, {
+        //TODO: this is buggy
         brightness,
         hex: domain.hex,
         hue,
@@ -98,6 +107,7 @@ export const SavePalette = () => {
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
+            <ChangesAccordion />
             <AlertDialogCancel style={{ borderColor: c1 }}>
               Cancel
             </AlertDialogCancel>
@@ -128,5 +138,104 @@ export const SavePalette = () => {
         </AlertDialogContent>
       )}
     </AlertDialog>
+  );
+};
+
+type Changes = {
+  [key in keyof Attributes]: { saved: string; modified: string };
+};
+
+const ChangesAccordion = () => {
+  const { domains } = usePersistentStore();
+
+  const mode = useDarkMode((state) => state.mode);
+  const name = useVariables((state) => state.name);
+  const domain = usePointersDomain();
+  const stage = usePointers((state) => state.stage);
+
+  const brightness = useVariables((state) => state.brightness);
+  const hue = useVariables((state) => state.hue);
+  const saturation = useVariables((state) => state.saturation);
+  const colorSpace = useVariables((state) => state.colorSpace);
+  let saved = domains[name];
+  const x: Changes = {
+    hue: {
+      modified: hue.toString(),
+      saved: saved.hue.toString(),
+    },
+    brightness: {
+      modified: brightness.toString(),
+      saved: saved.brightness.toString(),
+    },
+
+    saturation: {
+      modified: saturation.toString(),
+      saved: saved.saturation.toString(),
+    },
+
+    space: {
+      modified: colorSpace.toString(),
+      saved: saved.space.toString(),
+    },
+
+    stage: {
+      modified: stage.toString(),
+      saved: saved.stage.toString(),
+    },
+  };
+
+  return (
+    <Accordion className="w-full" type="single" collapsible>
+      <AccordionItem value="??" className="border-none mt-2">
+        <AccordionTrigger className="p-0 font-bold" decoration={false}>
+          See your changes
+        </AccordionTrigger>
+        <AccordionContent>
+          <div className="flex flex-col">
+            {Object.keys(x).map((k: string) => {
+              let asd = x[k as keyof Attributes];
+              return (
+                <span>
+                  {asd.saved !== asd.modified && (
+                    <>
+                      {k}:{" "}
+                      <span className="font-bold text-red-600">
+                        {asd.saved}
+                      </span>
+                      <span className="font-bold text-xs mx-1 tracking-tighter">
+                        ..
+                      </span>
+                      <span className="font-bold text-green-600">
+                        {asd.modified}
+                      </span>
+                    </>
+                  )}
+                </span>
+              );
+            })}
+            {/* TODO: both index conditions should be considered ONLY when the stage is setted to shade */}
+            {/* FIXME: refactor the whole file */}
+            {/* TODO: match only for valid pointers (the middle pointer in case of shade stage) */}
+            <span className="flex flex-col">
+              colors:
+              <span>
+                {domain.hex.map((hex: string, i: number) => {
+                  if (i > 0 && i < domain.hex.length - 1) {
+                    return <span>{hex}</span>;
+                  }
+                })}
+              </span>
+              <span>
+                {domains[name].hex.map((hex: string, i: number) => {
+                  if (i > 0 && i < domains[name].hex.length - 1) {
+                    return <span>{hex}</span>;
+                  }
+                })}
+              </span>
+            </span>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 };
